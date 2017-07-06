5fc95538f6b1adeccd97164e46d5810a31cc7049
RicardoSzram <ricardo.szram.filho@gmail.com>
2017-07-06 13:45:48 -0300

    Inicial

---
+++ main.c
@@ -0,0 +1,31 @@
+/*
+Autores: Italo Antonio RA: 744337, Ricardo Szram RA: 744353 e Vitor Ferreira RA: 744357
+Revisor: Prof. Ricardo Rodrigues Ciferri.
+Data: 19/07/2017
+Objetivo:
+- Criar um jogo da memória utilizando CGI.
+*/
+
+
+// Bibliotecas
+#include <stdio.h>
+#include <stdlib.h>
+
+// Constantes
+
+
+// Novos tipos de dados
+
+
+// Protótipos
+
+
+// Programa Principal
+
+int main(int argc, char *argv[]) {
+	
+	return 0;
+}
+
+// Sub rotinas
+
